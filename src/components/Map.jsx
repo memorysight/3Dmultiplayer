e6d4ae7dc9ce87735f6f368189549a6ca0977248
Daniel Smith <memorysight@gmail.com>
2024-05-14 17:06:45 -0400

    initial commit off master

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,23 +1,21 @@
 import { useGLTF } from "@react-three/drei";
-import { RigidBody } from "@react-three/rapier";
-import { useEffect } from "react";
+// import { RigidBody } from "@react-three/rapier";
+// import { useEffect } from "react";
 
-export const Map = () => {
+
+function Map(){
+
   const map = useGLTF("models/map.glb");
-  useEffect(() => {
-    map.scene.traverse((child) => {
-      if (child.isMesh) {
-        child.castShadow = true;
-        child.receiveShadow = true;
-      }
-    });
-  });
+
   return (
     <>
-      <RigidBody colliders="trimesh" type="fixed">
-        <primitive object={map.scene} />
-      </RigidBody>
+     <primitive object={map.scene} />
     </>
-  );
-};
+   
+  )
+
+
+}
 useGLTF.preload("models/map.glb");
+
+export default Map;
